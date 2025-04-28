7a72f2a03518c4bc43fb411e13f4d5c7cccf18e8
pancake5000 <jkwiatkowski987@gmail.com>
2025-04-29 00:19:56 +0200

    handle input, make message structs(partially)

---
+++ komunikaty.h
@@ -0,0 +1,36 @@
+#include <vector>
+typedef struct
+{
+    char message;
+    message_data data;
+} message_t;
+
+typedef union
+{
+    nodata_t nodata;
+    timestamp_t timestamp;
+    peers_t peers;
+} message_data;
+
+typedef struct{
+
+}nodata_t;
+
+typedef struct
+{
+    unsigned long long timestamp;
+    char sychronized;
+} timestamp_t;
+
+typedef struct
+{
+   unsigned short count;
+   std::vector<peer> peers;
+} peers_t;
+
+typedef struct
+{
+    char peer_address_length;
+    std::vector<char> peer_address;
+    unsigned short port;
+} peer;
--- serwer.cpp
+++ serwer.cpp
@@ -1,35 +1,128 @@
-#include<iostream>
-#include<unistd.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<arpa/inet.h>
-#include<cstring>
-#include<cerrno>
+#include <iostream>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <cstring>
+#include <cerrno>
 
+#include "komunikaty.h"
 using namespace std;
 
-int main(int argc, char *argv[])
+int read_from_argv(int argc, char *argv[], string &bind_address, int &port, string &peer_address, int &peer_port)
 {
     int opt;
-    string bind_address = "0.0.0.0"; // Default: listen on all addresses
-    int port = 0;                   // Default: any available port
-    string peer_address = "";       // Default: no peer address
-    int peer_port = 0;              // Default: no peer port
-    
-    while((opt = getopt(argc, argv, "b:p:a:r"))){
-        switch (opt){
-            case 'b':
-                bind_address = optarg;
-                break;
-            case 'p':
-                port = std::stoi(optarg);
-                break;
-            case 'a':
-                peer_address = optarg;
-                break;  
-            case 'r':
-                peer_port = std::stoi(optarg);
-                break;
+    bool got_peer = 0;
+    bool got_peer_port = 0;
+    while ((opt = getopt(argc, argv, "b:p:a:r:")) != -1)
+    {
+        switch (opt)
+        {
+        case 'b':
+        {
+            if (optarg == nullptr)
+            {
+                cerr << "Error: Bind address is required." << endl;
+                return 1;
+            }
+            bind_address = optarg;
+            // Validate the bind address format (IPv4)
+            struct sockaddr_in sa;
+            if (inet_pton(AF_INET, bind_address.c_str(), &(sa.sin_addr)) == 0)
+            {
+                cerr << "Error: Invalid bind address format. Must be a valid IPv4 address." << endl;
+                return 1;
+            }
+            break;
+        }
+        case 'p':
+        {
+            if (optarg == nullptr)
+            {
+                cerr << "Error: Port is required." << endl;
+                return 1;
+            }
+            int tmp_port = 0;
+            try
+            {
+                tmp_port = std::stoi(optarg);
+            }
+            catch (const std::invalid_argument &e)
+            {
+                cerr << "Error: Invalid port number." << endl;
+                return 1;
+            }
+            catch (const std::out_of_range &e)
+            {
+                cerr << "Error: Invalid port number." << endl;
+                return 1;
+            }
+            port = tmp_port;
+            if (port < 0 || port > 65535)
+            {
+                cerr << "Error: Invalid port number." << endl;
+                return 1;
+            }
+            break;
+        }
+        case 'a':
+        {
+            if (optarg == nullptr)
+            {
+                cerr << "Error: No peer adsress" << endl;
+                return 1;
+            }
+            got_peer = 1;
+            peer_address = optarg;
+            struct sockaddr_in sa;
+            if (inet_pton(AF_INET, peer_address.c_str(), &(sa.sin_addr)) == 0)
+            {
+                cerr << "Error: Invalid bind address format. Must be a valid IPv4 address." << endl;
+                return 1;
+            }
+            break;
+        }
+        case 'r':
+        {
+            if (optarg == nullptr)
+            {
+                cerr << "Error: Peer port is required." << endl;
+                return 1;
+            }
+            got_peer_port = 1;
+            int tmp_port = 0;
+            try
+            {
+                tmp_port = std::stoi(optarg);
+            }
+            catch (const std::invalid_argument &e)
+            {
+                cerr << "Error: Invalid port number." << endl;
+                return 1;
+            }
+            catch (const std::out_of_range &e)
+            {
+                cerr << "Error: Invalid port number." << endl;
+                return 1;
+            }
+            port = tmp_port;
+            if (port < 0 || port > 65535)
+            {
+                cerr << "Error: Invalid port number." << endl;
+                return 1;
+            }
+            break;
+        }
+        default:
+        {
+            cerr << "Error: Invalid option: " << (char)opt << endl;
+            return 1;
+        }
+        }
+        if (got_peer != got_peer_port)
+        {
+            cerr << "Error: Peer address and port must be provided together." << endl;
+            return 1;
         }
     }
 
@@ -38,10 +131,30 @@ int main(int argc, char *argv[])
     cout << "Port: " << port << endl;
     cout << "Peer Address: " << peer_address << endl;
     cout << "Peer Port: " << peer_port << endl;
+    return 0;
+}
+int main(int argc, char *argv[])
+{
 
+    string bind_address = "0.0.0.0"; // Default: listen on all addresses
+    int port = 0;                    // Default: any available port
+    string peer_address = "";        // Default: no peer address
+    int peer_port = 0;               // Default: no peer port
+
+    if (read_from_argv(argc, argv, bind_address, port, peer_address, peer_port) != 0)
+    {
+        cerr << "Error: Invalid arguments." << endl;
+        return 1;
+    }
+    if (port <= 0 || port > 65535)
+    {
+        cerr << "Error: Invalid port number. Port must be between 1 and 65535." << endl;
+        return 1;
+    }
     // Initialize socket for binding (UDP and IPv4)
     int server_socket = socket(AF_INET, SOCK_DGRAM, 0);
-    if (server_socket < 0) {
+    if (server_socket < 0)
+    {
         cerr << "Error creating socket: " << strerror(errno) << endl;
         return 1;
     }
@@ -51,28 +164,39 @@ int main(int argc, char *argv[])
     server_addr.sin_port = htons(port);
     server_addr.sin_addr.s_addr = inet_addr(bind_address.c_str());
 
-    if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
         cerr << "Error binding socket: " << strerror(errno) << endl;
         return 1;
     }
 
     cout << "Server socket initialized and bound to " << bind_address << ":" << port << endl;
 
+    // Say Hello
+    // Wait for hello_reply
+    // connect to all the peers
+    /*
+        while(true){
+            czytaj z socketu
+            responduj
+        }
+    */
     // Initialize socket for peer connection if peer_address and peer_port are provided
-    if (!peer_address.empty() && peer_port > 0) {
+    if (!peer_address.empty() && peer_port > 0)
+    {
         sockaddr_in peer_addr{};
         peer_addr.sin_family = AF_INET;
         peer_addr.sin_port = htons(peer_port);
         peer_addr.sin_addr.s_addr = inet_addr(peer_address.c_str());
 
         // Example: Send a test message to the peer
-        string message = "Hello, peer!";
-        if (sendto(server_socket, message.c_str(), message.size(), 0, 
-                   (struct sockaddr*)&peer_addr, sizeof(peer_addr)) < 0) {
-            cerr << "Error sending message to peer: " << strerror(errno) << endl;
-            return 1;
-        }
+        // string message = "Hello, peer!";
+        // if (sendto(server_socket, message.c_str(), message.size(), 0,
+        //            (struct sockaddr*)&peer_addr, sizeof(peer_addr)) < 0) {
+        //     cerr << "Error sending message to peer: " << strerror(errno) << endl;
+        //     return 1;
+        // }
 
-        cout << "Message sent to peer at " << peer_address << ":" << peer_port << endl;
+        // cout << "Message sent to peer at " << peer_address << ":" << peer_port << endl;
     }
 }
